0c5fd695e58d0ff53771152197c98f6670be070e
lalitdhakar909 <117665519+lalitdhakar909@users.noreply.github.com>
2024-12-14 14:59:02 +0530

    Initial commit

---
+++ Uber-AuthService/src/main/java/Uber/Uber_AuthService/UberAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package Uber.Uber_AuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAuthServiceApplication.class, args);
+	}
+
+}
